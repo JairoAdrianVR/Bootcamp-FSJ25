6702e87df4d301d7825b1bf1b9407ca6717b9c21
JairoAdrianVG <jairoavegaromero@gmail.com>
2024-08-22 21:21:53 -0600

    Introduccion a Javascript - Clase 22/8/24

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,2 +1,24 @@
 {}
 ""
+
+// COMENTAR EN UNA LINEA -> DESACTIVAR UNA LINEA PARA EL ENTORNO DE EJECUCION
+/* 
+    COMENTARIO
+    MULTI
+    LINEAS
+*/
+
+
+// El concepto del lenguaje
+/* Caracteristicas:
+* Tipo de tipado: Debilmente Tipado -> LA FORMA DE ESCRIBIR (Nos perdona errores)
+* Tipado debil NO NECESITA:
+- CIERRES INSTRUCCION ;
+- ACLARAR EL TIPO DE DATO DE LAS VARIABLES 
+* PARADIGMA DE PROGRAMACION -> FORMA DE ESCRIBIR EL CODIGO: Orientado a objetos (prototipos)
+* FORMA DE ESCRIBIR CODIGO ES PROGRAMACION ESTRUCTURADA (FUNCIONES)
+* ENTORNO DE EJECUCION: Interpretado (Ejecutado en el navegador)
+*/
+
+
+// 
