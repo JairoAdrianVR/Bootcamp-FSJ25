d1373647f7236a2238111b782757ed0e186f8cce
JairoAdrianVG <jairoavegaromero@gmail.com>
2024-09-09 20:25:00 -0600

    Introduccion a TS - Introduccion a POO - Clase 9/9/24

---
+++ Typescript/POO/index.ts
@@ -0,0 +1,23 @@
+// Programacion Orientada a Objetos
+// Es una forma de programar intentando siempre describir o pensar las cosas como si fueran objetos
+
+// Conceptos Principales de POO -> Clases y los Objetos
+// Clases -> Molde para generar algo
+// Objeto -> Lo que podemos crear en base a ese molde
+
+//Declaracion de clase
+
+class Auto{
+    //Caracteristicas generales -> Propiedades o atributos
+    numChasis:number;
+    motor:string;
+    color:string;
+    tipoCombustible:string;
+    transmision:string;
+    cantKilometraje:string;
+    plazas:number;
+    frenos:string;
+    modelo:string;
+    anioFab:number;
+
+}
