7ac6fe469747f2406302a985c3109cf447f1f62b
JairoAdrianVG <jairoavegaromero@gmail.com>
2024-09-11 22:18:39 -0600

    Pilares POO - Encapsulamiento y Abstraccion - Clases 11/9/24

--- Typescript/POO/index.js
+++ Typescript/POO/index.js
@@ -1,28 +1,12 @@
 // Programacion Orientada a Objetos
 // Es una forma de programar intentando siempre describir o pensar las cosas como si fueran objetos
-
 // Conceptos Principales de POO -> Clases y los Objetos
 // Clases -> Molde para generar algo
 // Objeto -> Lo que podemos crear en base a ese molde
-
 //Declaracion de clase
-
-class Auto{
-
-    //Caracteristicas generales -> Propiedades o atributos
-    numChasis:number;
-    motor:string;
-    color:string;
-    tipoCombustible:string;
-    transmision:string;
-    cantKilometraje:string;
-    plazas:number;
-    frenos:string;
-    modelo:string;
-    anioFab:number;
-
+var Auto = /** @class */ (function () {
     //Constructor -> Metodo reservado que nos sirve para instanciar objetos
-    constructor(numChasisParam:number,motorParam:string,colorParam:string,tipoCompustibleParam:string, transmisionParam:string,plazasParam:number,frenosParam:string, modeloParam:string, añoFabParam:number){
+    function Auto(numChasisParam, motorParam, colorParam, tipoCompustibleParam, transmisionParam, plazasParam, frenosParam, modeloParam, añoFabParam) {
         this.numChasis = numChasisParam;
         this.motor = motorParam;
         this.color = colorParam;
@@ -33,26 +17,20 @@ class Auto{
         this.modelo = modeloParam;
         this.anioFab = añoFabParam;
     }
-
     //Metodos -> Acciones 
-    encender():void {
+    Auto.prototype.encender = function () {
         console.log("Brum re brum");
-    }    
-
-    apagar():void{
+    };
+    Auto.prototype.apagar = function () {
         console.log("Chauchis");
-    }
-    
-}
-
+    };
+    return Auto;
+}());
 //Instanciar objetos a traves de una clase -> Crear un objeto
-
-let autito:Auto = new Auto(123456,"v8","Azul","Gasolina","Manual",2,"ABC","Huayra",2018);
+var autito = new Auto(123456, "v8", "Azul", "Gasolina", "Manual", 2, "ABC", "Huayra", 2018);
 console.log(autito);
-
 //Asignamos una propiedad al objeto
 //autito.modelo = "Huayra";
 //console.log(autito);
-
 //autito.motor = "v12"
 //console.log(autito);
