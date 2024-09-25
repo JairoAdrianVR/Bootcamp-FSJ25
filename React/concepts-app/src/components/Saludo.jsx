12ffea27da2612a2fc789f7118d562f76aaf4f67
JairoAdrianVG <jairoavegaromero@gmail.com>
2024-09-24 21:42:07 -0600

    Conceptos Basicos React - Clase 24/9/24

---
+++ React/concepts-app/src/components/Saludo.jsx
@@ -0,0 +1,10 @@
+
+//Declaracion de un COMPONENTE
+export const Saludo = ()=> {
+  
+    return (
+      <>
+        <h3>Yo en realidad estoy en el componente Saludo jejox</h3>
+      </>
+    )
+ }
