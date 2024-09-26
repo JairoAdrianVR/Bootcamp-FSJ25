ad088ff40268e876a145b4320c007dc48a6b2be4
JairoAdrianVG <jairoavegaromero@gmail.com>
2024-09-25 23:57:02 -0600

    Estado - Clase 25/09/24

--- React/concepts-app/src/components/Saludo.jsx
+++ React/concepts-app/src/components/Saludo.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 
 //Declaracion de un COMPONENTE
 export const Saludo = ()=> {
-  
+  //let nombre = "Jairo";
+
+  //Primer HOOK -> useState
+  //Hook -> Es una funcion prehecha para realizar x accion
+  //useState(VALOR INICIAL )
+  const [nombre,cambiarNombre] = useState("Jairo");
+
     return (
       <>
         <h3>Yo en realidad estoy en el componente Saludo jejox</h3>
+        <h2>Hola {nombre}, como estas?</h2>
+        <button onClick={() => { cambiarNombre("Hector") }} >MAGIA </button>
+
       </>
     )
  }
