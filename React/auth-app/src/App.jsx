39c6e701c83a4e2bafa1118081ed0de6638aa29b
JairoAdrianVG <jairoavegaromero@gmail.com>
2024-09-30 20:53:22 -0600

    Auth Project - Clase 30/09/24

---
+++ React/auth-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './assets/css/App.css'
+import { Session } from './pages/session/Session'
+
+function App() {
+
+  return (
+    <>
+      <Session />
+    </>
+  )
+}
+
+export default App
---
+++ React/auth-app/src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCX6pKiRLQimiopnl1_omCR9RlK11B5Wzs",
+  authDomain: "authentic-app-a5edd.firebaseapp.com",
+  projectId: "authentic-app-a5edd",
+  storageBucket: "authentic-app-a5edd.appspot.com",
+  messagingSenderId: "620291887047",
+  appId: "1:620291887047:web:885b5cb66820f142f0d3e2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
