3edef259497e2ea93d8dd3a0c21e4aa36b2bbe68
JairoAdrianVG <jairoavegaromero@gmail.com>
2024-09-30 21:23:58 -0600

    Auth Project Firebase Config - Clase 30/09/24

--- React/auth-app/src/firebase/config.js
+++ React/auth-app/src/firebase/config.js
@@ -5,12 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCX6pKiRLQimiopnl1_omCR9RlK11B5Wzs",
-  authDomain: "authentic-app-a5edd.firebaseapp.com",
-  projectId: "authentic-app-a5edd",
-  storageBucket: "authentic-app-a5edd.appspot.com",
-  messagingSenderId: "620291887047",
-  appId: "1:620291887047:web:885b5cb66820f142f0d3e2"
+ //Datos de tu CONFIG de FIREBASE
 };
 
 // Initialize Firebase
