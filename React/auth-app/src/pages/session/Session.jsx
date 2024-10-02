37af975280beb636f687d821aaef957a0919a9d4
JairoAdrianVG <jairoavegaromero@gmail.com>
2024-10-01 20:17:26 -0600

    Register Auth Firebase - Clase 1/10/24

--- React/auth-app/src/pages/session/Session.jsx
+++ React/auth-app/src/pages/session/Session.jsx
@@ -1,25 +1,12 @@
-import {useForm} from 'react-hook-form'
+import { RegisterFormComponent } from "./components/RegisterFormComponent"
 
 export const Session = () => {
-    const {register} = useForm();
-
-    const onSubmitForm = (e) => {
-        e.preventDefault();
-        console.log("Se envio el form");
-    }
+    
 
   return (
-    <div>
-        <form action={(e) => onSubmitForm(e)}>
-            <label> Email</label>
-            <input type="text" id="email" placeholder="Example: Email@mail.com"/>
-            <label> Password</label>
-            <input type="password" id="password" placeholder="Enter your password"/>
-            <label>Confirm Password</label>
-            <input type="password" id="confirmPassword" placeholder="Repeat your password"/>
-            
-            <button type="submit">Send</button>
-        </form>
-    </div>
+   <>
+      <h1>Manejo de sesion</h1>
+      <RegisterFormComponent />
+   </>
   )
 }
