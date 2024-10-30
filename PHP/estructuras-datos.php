e8a8f1a69fb3efff9955d2d22cddabe552b73634
JairoAdrianVR <jairoavegaromero@gmail.com>
2024-10-29 18:17:47 -0600

    Clase 28/10/24

---
+++ PHP/estructuras-datos.php
@@ -0,0 +1,18 @@
+<?php 
+
+    //Estructuras de datos
+    //Arrays
+    
+    //Declaraciones de arrays
+    //Inicializacion a traves de constructor de objeto
+    $array = new ArrayObject();
+   
+    //Declaracion de array literal
+    $array = [];
+
+    //Declaracion de array a traves de un metodo
+    $array = array();
+
+    
+
+?> 
--- PHP/index.php
+++ PHP/index.php
@@ -27,15 +27,54 @@
 
 
     //Operadores 
-    //Operador de concatenacion
+    // Operador de concatenacion
     print($nombre.$apellido."\n");
 
-    //Operador matematico
+    // Operador matematico
     $suma = $numero + $decimal;
     $resta = $numero - $decimal;
     $multiplicacion = $numero * $decimal;
     $division = $numero / $decimal;
     $residuo = $numero % $decimal;
 
+    //Operadores Comparacion
+    $igualdad = 5 == "5";
+    $igualdadEstricta = 5 === "5";
+    $desigualdad = 5 != "5";
+    $desigualdadEstricta = 5 !== "5";
 
+    $mayorQue = 5 > 5;
+    $menorQue = 5 < 5;
+    $mayorIgual = 5 >= 5;
+    $menorIgual = 5 <= 5;
+
+    //Operadores Logicos
+    // AND &&
+    // OR ||
+
+    //Funciones 
+    //Funciones Expresadas
+    function Saludar(){
+        print("Holiwis \n");
+    }
+
+    function Despedir(){
+        return "Chauchis \n";
+    }
+
+    $resultado = Saludar();
+    $resulta2 = Despedir();
+    print("Este es el resultado: {$resulta2} \n");
+
+    //Funciones anonimas
+    $funcioncita = function() { print("Soy anonima jeje");};
+
+    //Funciones flecha
+    $flechita = fn() => ( print("Soy una funcion anonima flechita jejejiji"));
+
+    //Llamado a funciones
+    $funcioncita();
+    $flechita();
+    Saludar();
+    Despedir();
 ?>
