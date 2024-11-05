3cb2a63981f3516f6a3a31f0963b8b81834dede8
JairoAdrianVR <jairoavegaromero@gmail.com>
2024-11-04 22:50:12 -0600

    Clase 4/11/24

--- PHP/estructuras-datos.php
+++ PHP/estructuras-datos.php
@@ -178,11 +178,47 @@
                 $aux->next = $newNode;
             }
         }
+
+        function delete($data){
+            if($this->head === null){
+                return "La lista estaba previamente vacia. \n";
+            }
+
+            if($this->head->value === $data){
+                print("Se Encontro. \n");
+                $this->head = $this->head->next;
+                return "Encontrado";
+            }
+            
+            
+            $aux = $this->head;
+
+
+            while($aux->next !== null ){
+               if($aux->next->value == $data){
+                $aux->next = $aux->next->next;
+                print("Si se elimino. \n");
+                return "Se ha eliminado el dato.";
+               }
+
+                $aux = $aux->next;
+            }
+
+            return "Ese dato no existe en la lista.";
+
+        }
+
+        // Generar un metodo el cual me imprima o retorne el length de la lista
+        
+
+        // Generar un metodo para vaciar la lista
+
     }
     
     $listita = new LinkedList();
     $listita->insert(3);
     $listita->insert(5);
     $listita->insert(100);
+    $listita->delete(5);
     print_r($listita);
     ?> 
