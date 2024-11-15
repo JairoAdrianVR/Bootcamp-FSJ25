591ad94e7a6c3f8ee7af24f758dd1c47304a83dc
JairoAdrianVR <jairoavegaromero@gmail.com>
2024-11-14 20:10:52 -0600

    InsertionSort - Clase 13/11/24

--- PHP/algoritmos.php
+++ PHP/algoritmos.php
@@ -34,4 +34,23 @@
 
     $arraycito = [8,10,5,40,7];
     print_r(bubbleSort($arraycito));
+
+
+    function insertionSort($arr){
+
+        if(count($arr) <= 1 ) return "Que gracioso que sos!";
+
+        for($i = 1; $i < count($arr); $i++){
+            $key = $arr[$i];
+
+            $j = $i - 1;
+            while($j >= 0 && $arr[$j]>$key){
+                $arr[$j+1] = $arr[$j];
+                $j--;
+            }
+            $arr[$j+1] = $key;
+        }  
+        return $arr;     
+    }
+
 ?>
