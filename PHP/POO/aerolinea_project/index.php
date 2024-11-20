fbf7d3cbc8948b02ccd0c379fde4820e3186584a
JairoAdrianVR <jairoavegaromero@gmail.com>
2024-11-19 21:30:42 -0600

    Create Aerolinea - Clase 19/11/24

---
+++ PHP/POO/aerolinea_project/index.php
@@ -0,0 +1,61 @@
+<?php 
+
+    //Importar un archivo
+    // include -> Traer codigo del archivo, SI ALGO FALLA? Da una advertencia y sigue ejecutando el programa
+    // require -> Traer codigo del archivo, SI ALGO FALLA? Da un error y frena la ejecucion
+    require('./aerolinea.php');
+    session_start();
+    
+    if(!isset($_SESSION['aerolineas'])){
+        $_SESSION['aerolineas'] = [];
+    }
+
+    $aerolineas =  $_SESSION['aerolineas'] ;
+
+    if(isset($_POST['nombre'],$_POST['cantAviones'],$_POST['tipoAerolinea'])){
+        //print_r($_POST);
+
+        $nombre = $_POST['nombre'];
+        $cantAviones = $_POST['cantAviones'];
+        $tipo = $_POST['tipoAerolinea'];
+
+        $aerolinea = new Aerolinea($nombre,$cantAviones,$tipo);
+        print_r($aerolinea);
+        array_push($aerolineas,$aerolinea);
+        echo "<br/>";
+        $_SESSION['aerolineas'] = $aerolineas;
+    }
+    
+    print_r($aerolineas);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD Aerolinea</title>
+</head>
+<body>
+    <h1>Holiwis</h1>
+
+    <form method="POST" action="">
+        <label>Nombre Aerolinea</label>
+        <input type="text" name="nombre">
+
+        <label>Cantidad de Aviones</label>
+        <input type="text" name="cantAviones">
+
+        <label>Tipo de Aerolinea</label>
+        <select name="tipoAerolinea">
+            <option value="Comercial">Comercial</option>
+            <option value="Privado">Privado</option>
+            <option value="Carga">Carga</option>
+        </select>
+
+        <button type="submit">Registrar Aerolinea</button>
+    </form>
+</body>
+</html>
